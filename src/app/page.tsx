4f6da921bf69f9b774a167c7e41b1b8e92aa7233
aakash2330 <aakash2330@gmail.com>
2024-09-28 23:59:19 +0530

    envs pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            aliquipex ea commodo consequat. Duis aute irure dolor in
           </p>
           <br />
           <div className="flex items-center justify-center">
